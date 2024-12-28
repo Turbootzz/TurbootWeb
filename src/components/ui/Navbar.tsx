7568130ac7d98170fb8a45ad9d734676625ec5d2
Thijs Herman <tc.herman@hotmail.com>
2024-12-28 21:17:53 +0100

    small fix

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
             </Link>
             <Link
               href='/signin'
-              className='rounded-md border-2 border-solid border-c_lightpurple px-3 py-2 text-c_black text-c_white hover:bg-c_lightpurple'
+              className='rounded-md border-2 border-solid border-c_lightpurple px-3 py-2 text-c_black hover:bg-c_lightpurple dark:text-c_white'
             >
               Sign in
             </Link>
